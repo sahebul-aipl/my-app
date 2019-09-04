cec60eb6feeee3a49ad1618117d61897a2ac4be9
sahebul <sahebul@avantikain.com>
2019-09-04 17:31:52 +0530

    Updates

---
+++ precache-manifest.978ad107f9585bb3393b9b4fab7d7e4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca190e938948a8006d6f6db5d47225e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "464a2deab1fdb849ff02",
+    "url": "/my-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4a730d5c2f1f8fd2b755",
+    "url": "/my-app/static/js/2.65e324cd.chunk.js"
+  },
+  {
+    "revision": "464a2deab1fdb849ff02",
+    "url": "/my-app/static/js/main.d2a7948f.chunk.js"
+  },
+  {
+    "revision": "00fa2de64e66be72c556",
+    "url": "/my-app/static/js/runtime~main.122bc8ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
